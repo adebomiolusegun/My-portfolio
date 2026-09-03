0273c31a751f0664f595e091a1c026c715948fbb
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-09-03 23:25:43 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+export default function page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
