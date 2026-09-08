4312c084e0110964e81cb5a32fe6b39ca05b7e87
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-09-08 11:49:28 +0100

    creating components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-export default function page() {
+import NavPage from "./nav/page";
+
+function page() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="">
+      <NavPage />
+    </main>
   );
 }
+
+export default page;
