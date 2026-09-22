2c071b46320c501e8e84639db1bf3f3ab770a60f
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-09-22 18:15:08 +0100

    working on nav

---
+++ app/components/Contact/page.tsx
@@ -0,0 +1,10 @@
+function ContactPage() {
+  return (
+    <main className="flex flex-col gap-6 h-screen justify-center  px-4 py-2">
+      <h1>Contact</h1>
+      <p>Get in touch with me!</p>
+    </main>
+  );
+}
+
+export default ContactPage;
---
+++ app/components/Header/page.tsx
@@ -0,0 +1,12 @@
+import NavPage from "../Nav/page";
+
+function Header() {
+  return (
+    <main className="flex flex-col items-center  text-white p-4">
+      <NavPage />
+      <h1>Welcome to My Website</h1>
+    </main>
+  );
+}
+
+export default Header;
