41fd46e1788757ba6fedc79633bd700758eebff2
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-09-23 20:02:47 +0100

    arranged fonts and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { JetBrains_Mono, Roboto } from "next/font/google";
 import type { ReactNode } from "react";
 
 import "./global.css";
@@ -6,10 +7,22 @@ export const metadata = {
   title: "my-portfolio",
 };
 
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${jetbrainsMono.variable} ${roboto.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
